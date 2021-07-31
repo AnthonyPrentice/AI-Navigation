cb8102d9cc1e20145696a43bf3875bc5198ebb16
Anthony Prentice <anthony.prentice1@gmail.com>
2021-07-31 16:44:07 -0700

    Updated repository of work I've done so far.

---
+++ src/GameEngine.py
@@ -0,0 +1,131 @@
+import pygame
+import math
+from  Sprites import *
+
+#configurations
+width, height = 1000, 700
+white, blue , black, red, green = ((230, 230, 230), (0, 0, 255), (0, 0, 0),
+                                 (255, 0, 0), (0, 128, 0))
+
+pygame.init()
+pygame.display.set_caption("AI Navigation Project")
+screen = pygame.display.set_mode(size=(width, height))
+clock = pygame.time.Clock()
+fps = 60
+pi = math.pi
+
+#objects
+wall_sprites = pygame.sprite.Group()
+car_sprites = pygame.sprite.Group()
+sensor_sprites =  pygame.sprite.Group()
+node_sprites = pygame.sprite.Group()
+walls = list()
+cars = list()
+nodes = list()
+
+#init walls
+for x in range(0,16):
+    walls.append(Wall())
+    wall_sprites.add(walls[x])
+
+#placing walls
+walls[0].move(200, 150), walls[0].resize(110, 10)
+walls[1].move(150, 350), walls[1].resize(10, 400)
+walls[2].move(250, 300), walls[2].resize(10, 300)
+walls[3].move(305, 445), walls[3].resize(100, 10)
+walls[4].move(305, 545), walls[4].resize(300, 10)
+walls[5].move(350, 290), walls[5].resize(10, 300)
+walls[6].move(450, 390), walls[6].resize(10, 300)
+walls[7].move(505, 145), walls[7].resize(300, 10)
+walls[8].move(505, 245), walls[8].resize(100, 10)
+walls[9].move(650, 300), walls[9].resize(10, 300)
+walls[10].move(550, 400), walls[10].resize(10, 300)
+walls[11].move(705, 445), walls[11].resize(100, 10)
+walls[12].move(705, 545), walls[12].resize(300, 10)
+walls[13].move(750, 290), walls[13].resize(10, 300)
+walls[14].move(850, 340), walls[14].resize(10, 400)
+walls[15].move(800, 140), walls[15].resize(110, 10)
+walls[15].color(red)
+walls[15].finish = True
+
+#cars
+cars.append(Car())
+car_sprites.add(cars[0])
+cars[0].move(200,250)
+cars[0].rotate(45)
+#car sensors
+for x in range(0, 4):
+    sensor_sprites.add(cars[0].sensors[x])
+
+#ai nodes
+for x in range(0, 7):
+    nodes.append(Node())
+    node_sprites.add(nodes[x])
+nodes[0].move(200, 500), nodes[0].color(green)
+nodes[1].move(400, 500), nodes[1].color(green)
+nodes[2].move(400, 200), nodes[2].color(green)
+nodes[3].move(600, 200), nodes[3].color(green)
+nodes[4].move(600, 500), nodes[4].color(green)
+nodes[5].move(800, 500), nodes[5].color(green)
+nodes[6].move(800, 130), nodes[6].color(green)
+
+#program running
+drive = True
+run = True
+#direction = "w"
+node = 0
+while run:
+    clock.tick(fps)
+    screen.fill(white)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    '''
+    if direction == "a":
+        cars[0].angle = pi
+        cars[0].drive()
+    elif direction == "s":
+        cars[0].angle = pi/2
+        cars[0].drive()
+    elif direction == "d":
+        cars[0].angle = 0
+        cars[0].drive()
+    elif direction == "w":
+        cars[0].angle = (3*pi)/2
+        cars[0].drive()
+    else:
+        pass
+    '''
+
+    if node < 7:
+        cars[0].angle = cars[0].getDirection(nodes[node])
+        cars[0].drive()
+        nodeCol = pygame.sprite.spritecollideany(cars[0], node_sprites)
+        if nodeCol:
+            nodes[node].move(-20, 1)
+            node += 1
+
+    #sensor detection
+    for sensor in cars[0].sensors:
+        sensor.calibrate(cars[0])
+        sensor.see(cars[0], wall_sprites)
+
+    #car collisions
+    col = pygame.sprite.spritecollideany(cars[0], wall_sprites)
+    if col:
+        cars[0].speed = 0
+        cars[0].image.fill(green)
+
+    #Displaying to screen
+    wall_sprites.update()
+    car_sprites.update()
+    sensor_sprites.update()
+    node_sprites.update()
+    wall_sprites.draw(screen)
+    car_sprites.draw(screen)
+    sensor_sprites.draw(screen)
+    node_sprites.draw(screen)
+
+    pygame.display.update()
+
+pygame.quit()
---
+++ src/Sprites.py
@@ -0,0 +1,145 @@
+import pygame
+import math
+
+width, height = 1000, 700
+white, blue , black, red, green = ((230, 230, 230), (0, 0, 255), (0, 0, 0),
+                                 (255, 0, 0), (0, 128, 0))
+#-------------------------------------------------------------------------------
+class Sprite():
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+    def rotate(self, angle):
+        self.image = pygame.transform.rotate(self.image, angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+    def move(self, x, y):
+        self.rect.center = (x, y)
+    def resize(self, width, height):
+        self.image = pygame.Surface((width, height))
+        self.rect = self.image.get_rect(center=self.rect.center)
+    def color(self, color):
+        self.image.fill(color)
+        self.rect = self.image.get_rect(center=self.rect.center)
+ #-------------------------------------------------------------------------------   
+class Wall(pygame.sprite.Sprite, Sprite):
+    image = pygame.Surface((100, 10))
+    image.fill(black)
+    rect = image.get_rect()
+    rect.center = (70, 20)
+    finish = False
+#-------------------------------------------------------------------------------
+class Car(pygame.sprite.Sprite, Sprite):
+    image = pygame.Surface((20, 20), pygame.SRCALPHA)
+    image.fill(blue)
+    rect = image.get_rect()
+    rect.center = (width/2, height/2)
+    speed = 2
+    angle = 0
+    sensors = list() 
+    def initSensors(self):
+        for x in range(0, 4):
+            self.sensors.append(Sensor())
+            if x == 0:
+                self.sensors[x].name = "right"
+            elif x == 1:
+                self.sensors[x].name = "top"
+                self.sensors[x].resize(1, 50)
+            elif x == 2:
+                self.sensors[x].name = "left"
+            elif x == 3:
+                self.sensors[x].name = "bottom"
+                self.sensors[x].resize(1, 50)
+            else:
+                pass
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.initSensors()
+    def drive(self): #angle is in radians
+        self.rect.centerx = self.rect.centerx + (self.speed*math.cos(self.angle))
+        self.rect.centery = self.rect.centery + (self.speed*math.sin(self.angle))
+    def getPos(self):
+        position = [self.rect.centerx, self.rect.centery]
+        return position
+    def getDirection(self, sprite):
+        x = (sprite.rect.centerx - self.rect.centerx)
+        y = (sprite.rect.centery - self.rect.centery)
+        theta = 0
+        if x > 0:
+            if y > 0:
+                theta = math.atan2(y, x)
+            elif y < 0:
+                theta = math.atan2(y, x) + 6.28319
+            elif y == 0:
+                theta = 0
+        elif x < 0:
+            if y > 0:
+                theta = math.atan2(y, x) + 3.14159
+            elif y < 0:
+                theta = math.atan2(y, x) + 3.14159
+            elif y == 0:
+                theta = 3.14159
+        elif x == 0:
+            if y > 0:
+                theta = 1.5708
+            elif y < 0:
+                theta = 4.71239
+            elif y == 0:
+                theta = 0
+        return theta
+#-------------------------------------------------------------------------------
+class Sensor(pygame.sprite.Sprite, Sprite):
+    image = pygame.Surface((50, 1))
+    image.fill(blue)
+    rect = image.get_rect()
+    rect.center = (70, 20)
+    name = None
+    def calibrate(self, car):
+        if self.name == "right":
+            self.rect.left = car.rect.right
+            self.rect.centery = car.rect.centery
+        elif self.name == "top":
+            self.rect.bottom = car.rect.top
+            self.rect.centerx = car.rect.centerx
+        elif self.name == "left":
+            self.rect.right = car.rect.left
+            self.rect.centery = car.rect.centery
+        elif self.name == "bottom":
+            self.rect.top = car.rect.bottom
+            self.rect.centerx = car.rect.centerx
+        else:
+            pass
+    def see(self, car, wall_sprites):
+        col = pygame.sprite.spritecollideany(self, wall_sprites)
+        if col:
+            if self.name == "right":
+                self.resize(self.rect.width-2, self.rect.height)
+            elif self.name == "top":
+                self.resize(self.rect.width, self.rect.height-2)
+            elif self.name == "left":
+                self.resize(self.rect.width-2, self.rect.height)
+            elif self.name == "bottom":
+                self.resize(self.rect.width, self.rect.height-2)
+            else:
+                pass
+        else:
+            if self.name == "right":
+                if self.rect.width < 50:
+                    self.resize(self.rect.width+2, self.rect.height)
+            elif self.name == "top":
+                if self.rect.height < 50:
+                    self.resize(self.rect.width, self.rect.height+2)
+            elif self.name == "left":
+                if self.rect.width < 50:
+                    self.resize(self.rect.width+2, self.rect.height)
+            elif self.name == "bottom":
+                if self.rect.height < 50:
+                    self.resize(self.rect.width, self.rect.height+2)
+            else:
+                pass
+        self.calibrate(car)
+#-------------------------------------------------------------------------------
+class Node(pygame.sprite.Sprite, Sprite):
+    image = pygame.Surface((5, 5))
+    image.fill(green)
+    rect = image.get_rect()
+    rect.center = (70, 20)
+
